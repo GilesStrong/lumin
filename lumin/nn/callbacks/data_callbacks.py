d9df0372f0e759e364e441fbac0536a142655472
GilesStrong <giles.strong@outlook.com>
2019-04-26 15:59:44 +0100

    Addign reweighter

--- lumin/nn/callbacks/data_callbacks.py
+++ lumin/nn/callbacks/data_callbacks.py
@@ -1,14 +1,19 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Callable, Optional
+import numpy as np
 
 from torch import Tensor
 
 from .callback import Callback
 from ..data.batch_yielder import BatchYielder
+from ..data.fold_yielder import FoldYielder
+from ...utils.misc import to_np, to_device
+from ..models.abs_model import AbsModel
 
 
 class BinaryLabelSmooth(Callback):
     '''Apply label smoothing to binary classes, based on arXiv:1512.00567'''
-    def __init__(self, coefs:Union[float,Tuple[float,float]]=0):
+    def __init__(self, coefs:Union[float,Tuple[float,float]]=0, model:Optional[AbsModel]=None):
+        super().__init__(model=model)
         self.coefs = coefs if isinstance(coefs, tuple) else (coefs, coefs)
     
     def on_epoch_begin(self, batch_yielder:BatchYielder, **kargs) -> None:
@@ -21,3 +26,18 @@ class BinaryLabelSmooth(Callback):
         '''Apply smoothing at test-time'''
         targets[targets == 0] = self.coefs[0]
         targets[targets == 1] = 1-self.coefs[1]
+
+
+class DynamicReweight(Callback):
+    def __init__(self, reweight:Callable[[Tensor], Tensor], scale:float=1e-2, model:Optional[AbsModel]=None):
+        super().__init__(model=model)
+        self.scale,self.reweight = scale,reweight
+    
+    def on_train_end(self, fy:FoldYielder, val_id:int, **kargs) -> None:
+        val_fld = fy.get_fold(val_id)
+        preds = self.model.predict_array(val_fld['inputs'], as_np=False)
+        coefs = to_np(self.reweight(preds, to_device(Tensor(val_fld['targets']))))
+        start_sum = np.sum(val_fld['weights'])
+        val_fld['weights'] += self.scale*coefs*val_fld['weights']
+        val_fld['weights'] *= start_sum/np.sum(val_fld['weights'])
+        fy.foldfile[f'fold_{val_id}/weights'][...] = val_fld['weights'].squeeze()
