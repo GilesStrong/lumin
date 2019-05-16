5bab6c63e5f1f100b38a15c0eb2c66f1a383d4ca
GilesStrong <giles.strong@outlook.com>
2019-05-16 14:34:35 +0100

    Removing leftover code

--- lumin/nn/models/blocks/body.py
+++ lumin/nn/models/blocks/body.py
@@ -7,52 +7,6 @@ from torch import Tensor
 
 from ..layers.activations import lookup_act
 from ..initialisations import lookup_normal_init
-
-
-class AbsBody(nn.Module):
-    def __init__(self, depth:int, width:int, do:float, bn:bool, act:str,
-                 lookup_init:Callable[[str,Optional[int],Optional[int]],Callable[[Tensor],None]]=lookup_normal_init,
-                 lookup_act:Callable[[str],nn.Module]=lookup_act, freeze:bool=False):
-        super().__init__()
-        self.depth,self.width,self.do,self.bn,self.act = depth,width,do,bn,act
-
-    def __getitem__(self, key:int) -> nn.Module: return self.layers[key]
-
-    def freeze_layers(self):
-        for p in self.parameters(): p.requires_grad = False
-    
-    def unfreeze_layers(self):
-        for p in self.parameters(): p.requires_grad = True
-
-    def get_out_size(self) -> int: return self.width    
-
-
-# class FullyConnected(AbsBody):
-#     '''Fully connected set of hidden layers'''
-#     def __init__(self, depth:int, width:int, do:float, bn:bool, act:str,
-#                  lookup_init:Callable[[str,Optional[int],Optional[int]],Callable[[Tensor],None]]=lookup_normal_init,
-#                  lookup_act:Callable[[str],nn.Module]=lookup_act, freeze:bool=False):
-#         super().__init__(depth=depth, width=width, do=do, bn=bn, act=act, lookup_init=lookup_init, lookup_act=lookup_act, freeze=freeze)
-#         self.layers = nn.ModuleList([self.get_layer(d) for d in range(depth)])
-#         if self.freeze: self.freeze_layers
-    
-#     def get_layer(self, idx:int, fan_in:Optional[int]=None, fan_out:Optional[int]=None) -> nn.Sequential:
-#         width = self.width if not self.dense else self.width*(2**idx)
-#         fan_in  = width if fan_in  is None else fan_in
-#         fan_out = width if fan_out is None else fan_out
-        
-#         layers = [nn.Linear(fan_in, fan_out)]
-#         self.lookup_init(self.act, fan_in, fan_out)(layers[-1].weight)
-#         if self.act != 'linear': layers.append(self.lookup_act(self.act))
-#         if self.bn: layers.append(nn.BatchNorm1d(fan_out))
-#         if self.do: 
-#             if self.act == 'selu': layers.append(nn.AlphaDropout(self.do))
-#             else:                  layers.append(nn.Dropout(self.do))
-#         return nn.Sequential(*layers)
-    
-#     def forward(self, x:Tensor) -> Tensor:
-#         for l in self.layers: x = l(x)
-#         return x
     
 
 class FullyConnected(nn.Module):
