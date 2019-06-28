7c1f9afc4b1c7487edaf0a306f97fa2d7e8124ad
GilesStrong <giles.strong@outlook.com>
2019-06-28 13:00:29 +0100

    activation doc

--- lumin/nn/models/layers/activations.py
+++ lumin/nn/models/layers/activations.py
@@ -1,9 +1,20 @@
+from typing import Any
+
 import torch
 import torch.nn as nn
 
 
-def lookup_act(act:str) -> nn.Module:
-    '''Map activation name to class'''
+def lookup_act(act:str) -> Any:
+    r'''
+    Map activation name to class
+    
+    Arguments:
+        act: string representation of activation function
+
+    Returns:
+        Class implementing requested activation function
+    '''
+
     if act == 'relu':       return nn.ReLU()
     if act == 'prelu':      return nn.PReLU()
     if act == 'selu':       return nn.SELU()
@@ -16,7 +27,16 @@ def lookup_act(act:str) -> nn.Module:
 
         
 class Swish(nn.Module):
-    '''Non-trainable Swish activation function https://arxiv.org/abs/1710.05941'''
+    r'''
+    Non-trainable Swish activation function https://arxiv.org/abs/1710.05941
+
+    Arguments:
+        inplace: whether to apply activation inplace
+
+    Examples::
+        >>> swish = Swish()
+    '''
+
     def __init__(self, inplace=False):
         super().__init__()
         self.inplace = True
@@ -24,7 +44,7 @@ class Swish(nn.Module):
     def forward(self, x):
         if self.inplace:
             x.mul_(torch.sigmoid(x))
-            return x
+            return x  # Do we need to return?
         else:
             return x*torch.sigmoid(x)
             
