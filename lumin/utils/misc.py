c09637688f491d22777d17e2a9351075a490dcf6
GilesStrong <giles.strong@outlook.com>
2019-07-08 22:28:09 +0200

    misc doc

--- lumin/utils/misc.py
+++ lumin/utils/misc.py
@@ -6,28 +6,81 @@ import sympy
 from torch.tensor import Tensor
 import torch
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')  # TODO: make device choosable by user
 
 
-def to_np(x:Tensor) -> np.ndarray: return x.cpu().detach().numpy()
+def to_np(x:Tensor) -> np.ndarray:
+    r'''
+    Convert Tensor x to a Numpy array
+
+    Arguments:
+        x: Tensor to convert
+
+    Returns:
+        x as a Numpy array
+    '''
+    
+    return x.cpu().detach().numpy()
 
 
-def to_device(x:Union[Tensor,List[Tensor]], device:torch.device=device):
+def to_device(x:Union[Tensor,List[Tensor]], device:torch.device=device) -> Union[Tensor,List[Tensor]]:
+    r'''
+    Recursively place Tensor(s) onto device
+
+    Arguments:
+        x: Tensor(s) to place on device
+
+    Returns:
+        Tensor(s) on device
+    '''
+
     if x is None: return x
     if isinstance(x, list): return [to_device(o, device) for o in x]
     return x.to(device)
 
 
-def to_tensor(x:np.ndarray) -> Union[Tensor, None]: return Tensor(x) if x is not None else None
+def to_tensor(x:Union[np.ndarray,None]) -> Union[Tensor, None]: 
+    r'''
+    Convert Numpy array to Tensor with possibility of a None being passed
+
+    Arguments:
+        x: Numpy array or None
+
+    Returns:
+        x as Tensor or None
+    '''
+    
+    return Tensor(x) if x is not None else None
 
 
-def str2bool(string:str) -> bool:
+def str2bool(string:Union[str,bool]) -> bool:
+    r'''
+    Convert string representation of Boolean to bool
+
+    Arguments:
+        string: string representation of Boolean (or a Boolean)
+
+    Returns:
+        bool if bool was passed else, True if lowercase string matches is in ("yes", "true", "t", "1")
+    '''
+
     if isinstance(string, bool): return string
     else:                        return string.lower() in ("yes", "true", "t", "1")
 
 
 def to_binary_class(df:pd.DataFrame, zero_preds:List[str], one_preds:List[str]) -> None:
-    '''Map class precitions back to a binary prediction'''
+    r'''
+    Map class precitions back to a binary prediction.
+    The maximum prediction for features listed in zero_preds is treated as the prediction for class 0, vice versa for one_preds.
+    The binary prediction is added to df in place as column 'pred'
+    
+    Arguments:
+        df: DataFrame containing prediction features
+        zero_preds: list of column names for predictions associated with class 0
+        one_preds: list of column names for predictions associated with class 0
+    '''
+
     zero = df[zero_preds].max(axis=1)[:, None]
     one = df[one_preds].max(axis=1)[:, None]
     tup = np.hstack((zero, one))
@@ -38,7 +91,19 @@ def to_binary_class(df:pd.DataFrame, zero_preds:List[str], one_preds:List[str]) 
 
 
 def ids2unique(ids: Union[List, np.ndarray]) -> np.ndarray:
-    '''Map list of integers to a unique number'''
+    r'''
+    Map a permutaion of integers to a unique number, or a 2D array of integers to unique numbers by row.
+    Returned numbers are unique for a given permutation of integers.
+    This is achieved by computing the product of primes raised to powers equal to the integers. Beacause of this, it can be easy to produce numbers which are
+    too large to be stored if many (large) integers are passed.
+
+    Arguments:
+        ids: (array of ) permutation(s) of integers to map
+
+    Returns:
+        (Array of ) unique id(s) for given permutation(s)
+    '''
+
     if not isinstance(ids, np.ndarray): ids = np.array(ids)[:,None]
     primes = np.broadcast_to(np.array([sympy.prime(i) for i in range(1, 1+ids.shape[1])]), ids.shape)
     return (primes**ids).prod(axis=-1)
