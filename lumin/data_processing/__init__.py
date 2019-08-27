804fe056c5963f3ffc99e7aef320f1d7e0be84b7
GilesStrong <giles.strong@outlook.com>
2019-08-27 14:28:10 +0100

    evaluations

--- lumin/data_processing/__init__.py
+++ lumin/data_processing/__init__.py
@@ -2,6 +2,4 @@ from .file_proc import *  # noqa F304
 from .hep_proc import *  # noqa F304
 from .pre_proc import *  # noqa F304
 
-from .. import data_processing  # noqa F304
-
 __all__ = [*file_proc.__all__, *hep_proc.__all__, *pre_proc.__all__]  # noqa F405
