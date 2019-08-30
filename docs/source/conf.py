53dee27e581613fc86d3c6cf3b95827bbacdeb05
GilesStrong <giles.strong@outlook.com>
2019-08-30 12:49:16 +0100

    Trying mock torch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,8 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
+autodoc_mock_imports = ["torch"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
