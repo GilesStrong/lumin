90a3c88aacb179d118f65745eb26c8fcf657aa03
GilesStrong <giles.strong@outlook.com>
2019-08-30 15:26:58 +0100

    style test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,6 @@ extensions = [
     "sphinx_rtd_theme",
 ]
 
-# autodoc_mock_imports = ["torch"]
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
