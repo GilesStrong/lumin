ac16d090c9efba5e065149f3e0a359bb84daf860
GilesStrong <giles.strong@outlook.com>
2019-09-04 10:20:56 +0100

    Adding frontpage description

--- lumin/optimisation/features.py
+++ lumin/optimisation/features.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from typing import List, Optional
+from typing import List, Optional, Dict, Any
 from fastprogress import progress_bar
 from rfpimp import importances
 
