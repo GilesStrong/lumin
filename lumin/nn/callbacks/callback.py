6cd989203abfc1674f82c0460b5312f4f9a17fcb
GilesStrong <giles.strong@outlook.com>
2021-05-20 14:54:26 +0200

    Fix mode check in pred begin

--- lumin/nn/callbacks/callback.py
+++ lumin/nn/callbacks/callback.py
@@ -37,5 +37,5 @@ class Callback(AbsCallback):
         self.plot_settings = plot_settings
     
     def on_pred_begin(self) -> None:
-        if self.wrapper is None:
-            raise AttributeError(f"The model for {type(self).__name__} callback has not been set. Please call set_wrapper before on_pred_begin.")
+        if self.model is None:
+            raise AttributeError(f"The model for {type(self).__name__} callback has not been set. Please call set_wrapper before on_model_begin.")
